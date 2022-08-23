b5ca87d9c8fbcd18525373241af3aa7b4f9360a9
Martín Gamboggi <martin_gamboggi@hotmail.com>
2022-08-23 20:20:32 -0300

    cambios2

--- src/main/java/com/example/Libreria/controller/LibroController.java
+++ src/main/java/com/example/Libreria/controller/LibroController.java
@@ -3,10 +3,9 @@ package com.example.Libreria.controller;
 import com.example.Libreria.entity.Libro;
 import com.example.Libreria.service.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class LibroController {
@@ -20,6 +19,22 @@ public class LibroController {
     @GetMapping("/saveLibro")
     public Libro saveLibro(@RequestBody Libro libro){return libroService.saveLibro(libro);}
 
+    @GetMapping("/libroAutor/{nombre}")
+    public List<Libro> getListByAutor(@PathVariable(name = "nombre")String nombre){
+        return libroService.getLibrosByAutor(nombre);
+    }
 
+    @GetMapping("/libroGenero/{nombre}")
+    public List<Libro> getListByGenero(@PathVariable(name = "nombre") String nombre) {
+        return libroService.getLibrosByGenero(nombre);
+    }
+
+    @PostMapping("/probarProductoDto")
+    public CompraDto getListByGenero(@RequestBody CompraDto compraDto) throws Exception {
+        if (compraDto.getProductoDtoList().isEmpty()){
+            throw new Exception("Enviaste la lista de productos vacía");
+        }
+        return compraDto;
+    }
 
 }
